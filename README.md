3d8d4e9d10c2f258c40cdf0dbf26e8e298f6e530
mclift <mclift@two-cats>
2017-02-06 21:08:28 +0000

    Update project to VS 2015; add to GitHub.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SimpleAStarExample
+
+The sample project for the blog post, [A Simple A* Path-Finding Example in C#](http://blog.two-cats.com/2014/06/a-star-example/).
---
+++ SimpleAStarExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleAStarExample")]
+[assembly: AssemblyDescription("A simple path-finding example by Mike Clift")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Two-Cats Software Ltd.")]
+[assembly: AssemblyProduct("SimpleAStarExample")]
+[assembly: AssemblyCopyright("Copyright © 2014 Two-Cats Software Ltd.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4c5b6390-ede2-443f-af50-79c5300747db")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
